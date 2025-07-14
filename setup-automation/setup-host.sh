71950a8fd7d50ef50c92b5b56f1a49504ca46476
Matthew Yee <myee@redhat.com>
2025-07-14 14:24:22 -0700

    Refactor setup-host.sh to comment out VM boot wait and SSH key generation steps for improved clarity and flexibility.

--- setup-automation/setup-host.sh
+++ setup-automation/setup-host.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-while [ ! -f /opt/instruqt/bootstrap/host-bootstrap-completed ]
-do
-    echo "Waiting for Instruqt to finish booting the VM"
-    sleep 1
-done
+# while [ ! -f /opt/instruqt/bootstrap/host-bootstrap-completed ]
+# do
+#     echo "Waiting for Instruqt to finish booting the VM"
+#     sleep 1
+# done
 
-agent variable set sandboxid $_SANDBOX_ID
+# agent variable set sandboxid $_SANDBOX_ID
 
-virsh start centos
+# virsh start centos
 
-echo "${SSHKEY}" > /root/.ssh/id_rsa
-chmod 600 /root/.ssh/id_rsa
-ssh-keygen -f /root/.ssh/id_rsa -y > /root/.ssh/id_rsa.pub
+# echo "${SSHKEY}" > /root/.ssh/id_rsa
+# chmod 600 /root/.ssh/id_rsa
+# ssh-keygen -f /root/.ssh/id_rsa -y > /root/.ssh/id_rsa.pub
 
-# This sleep call is to give libvirt-nss time to find the ip address of the vm.
-sleep 30
+# # This sleep call is to give libvirt-nss time to find the ip address of the vm.
+# sleep 30
 
 # Attempt to register the vm 3 times, waiting 30 seconds for each attempt.
 #(r=3;while ! ssh -i /root/.ssh/id_rsa -o "StrictHostKeyChecking no" centos "subscription-manager register --activationkey=${ACTIVATION_KEY} --org=12451665 --force" ; do ((--r))||exit;sleep 30;done)
